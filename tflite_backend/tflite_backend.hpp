e856298ffb5d03204f6eb46ad7e4dd7d92656a84
Andrey Malyshev <elvin.nnov@gmail.com>
2022-02-24 13:35:04 +0300

    Fix of tflite linking options

--- tflite_backend/tflite_backend.hpp
+++ tflite_backend/tflite_backend.hpp
@@ -14,7 +14,7 @@ Backend* createBackend();
 
 class TFLiteBackend : public Backend {
 public:
-    virtual bool loadModel(const std::string &model, const std::string &device,
+    virtual bool loadModel(const VLauncher *launcher, const std::string &device,
                            const std::vector<std::string> &outputs,
                            const std::map<std::string, std::string>& config)override;
     virtual std::shared_ptr<InferenceMetrics> process(bool streamOutput = false)override
